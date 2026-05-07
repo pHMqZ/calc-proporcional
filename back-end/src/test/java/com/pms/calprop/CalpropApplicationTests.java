2ede7517e936f18690b90218579548347bf851bc
Phillip Marques <phillipmarq@hotmail.com>
2026-05-07 15:52:57 -0300

    chore: Adjusts in CI file

--- back-end/src/test/java/com/pms/calprop/CalpropApplicationTests.java
+++ back-end/src/test/java/com/pms/calprop/CalpropApplicationTests.java
@@ -2,7 +2,9 @@ package com.pms.calprop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("uat")
 @SpringBootTest
 class CalpropApplicationTests {
 
