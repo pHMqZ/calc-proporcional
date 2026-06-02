7b4463f224e5ab343f7a1527f19721485c3818fd
Phillip Marques <phillipmarq@hotmail.com>
2026-06-02 16:47:46 -0300

    dynamic url adjust for CORS

--- back-end/src/main/java/com/pms/calprop/config/WebConfig.java
+++ back-end/src/main/java/com/pms/calprop/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.pms.calprop.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Override
     @SuppressWarnings("null")
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
