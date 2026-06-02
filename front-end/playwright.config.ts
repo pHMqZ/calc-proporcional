3430c613a540c0732c4be7003517a82f5665d5b0
Phillip Marques <phillipmarq@hotmail.com>
2026-06-01 21:49:52 -0300

    ci: adjust in pipe archives

--- front-end/playwright.config.ts
+++ front-end/playwright.config.ts
@@ -34,10 +34,16 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['list'],
-    ['html']
-  ],
+  reporter: process.env.CI
+    ? [
+        ['list'],
+        ['github'],
+        ['html']
+      ]
+    : [
+        ['list'],
+        ['html']
+      ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
